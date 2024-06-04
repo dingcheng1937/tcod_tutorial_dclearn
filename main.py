31b7cc141db916372dbe775fe4d276492506243a
dingcheng1937 <dcrlikercd@126.com>
2024-06-04 12:36:21 +0800

    implement entity NPC in part2

--- main.py
+++ main.py
@@ -1,52 +1,36 @@
 import tcod
 
-from actions import Action, ActionType
-from input_handlers import handle_keys
+from engine import Engine
+from entity import Entity
+from input_handlers import EventHandler
 
 
-def main():
+def main() -> None:
     screen_width: int = 80
     screen_height: int = 50
 
-    player_x: int = int(screen_width / 2)
-    player_y: int = int(screen_height / 2)
-
-    tcod.console_set_custom_font("arial10x10.png", tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
-
-    with tcod.console_init_root(
-            w=screen_width,
-            h=screen_height,
-            title="Yet Another Roguelike Tutorial",
-            order="F",
-            vsync=True
-    ) as root_console:
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+    event_handler = EventHandler()
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Yet Another Roguelike Tutorial",
+        vsync=True,
+    ) as context:
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
+            engine.render(console=root_console, context=context)
 
-            tcod.console_flush()
+            events = tcod.event.wait()
 
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                if event.type == "QUIT":
-                    raise SystemExit()
-
-                if event.type == "KEYDOWN":
-                    action: [Action, None] = handle_keys(event.sym)
-
-                    if action is None:
-                        continue
-
-                    action_type: ActionType = action.action_type
-
-                    if action_type == ActionType.MOVEMENT:
-                        dx: int = action.kwargs.get("dx", 0)
-                        dy: int = action.kwargs.get("dy", 0)
-
-                        player_x += dx
-                        player_y += dy
-                    elif action_type == ActionType.ESCAPE:
-                        raise SystemExit()
+            engine.handle_events(events)
 
 
 if __name__ == "__main__":
