b6b6d4cfe9a059217da783b4051d4520925b3586
dingcheng1937 <dcrlikercd@126.com>
2024-06-05 13:42:50 +0800

    complete part7

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import copy
 import tcod
-
+import color
 from engine import Engine
 import entity_factories
 from game_map import GameMap
@@ -14,7 +14,7 @@ def main() -> None:
     screen_height: int = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -37,6 +37,9 @@ def main() -> None:
         engine=engine,
     )
     engine.update_fov()
+    engine.message_log.add_message(
+        "Hello and welcome, adventurer, to yet another dungeon!", color.welcome_text
+    )
 
     with tcod.context.new_terminal(
         screen_width,
@@ -47,10 +50,10 @@ def main() -> None:
     ) as context:
         root_console = tcod.console.Console(screen_width, screen_height, order="F")
         while True:
-            engine.render(console=root_console, context=context)
-
-            engine.event_handler.handle_events()
-
+            root_console.clear()
+            engine.event_handler.on_render(console=root_console)
+            context.present(root_console)
+            engine.event_handler.handle_events(context)
 
 if __name__ == "__main__":
     main()
